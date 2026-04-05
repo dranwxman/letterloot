e8dadfe62febdc86e98fc93cffb77d3664300fac
dranwxman <dranwxman@gmail.com>
2026-04-05 08:18:01 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <h1 style={{color:'red', padding:'40px'}}>HELLO</h1>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
